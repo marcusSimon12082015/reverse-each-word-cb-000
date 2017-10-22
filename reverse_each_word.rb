469b53c7c5917de29912931316dfca5b3883ff7c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 16:47:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   new_string = ""
   my_array = sentence.split(" ")
   my_array.each do | word |
-    new_string += word.reverse
+    new_string += "#{word.reverse} "
   end
   return new_string
 end
